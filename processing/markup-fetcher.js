3d759582652aecbe1b45ed90d85115906a8cb552
Sharon Funke <sharon.funke@nzz.ch>
2017-01-03 12:34:00 +0100

    Added stripping of item properties

--- processing/markup-fetcher.js
+++ processing/markup-fetcher.js
@@ -1,12 +1,12 @@
 const Fetch = require('node-fetch');
 //TODO: at the moment environment is fixed, has to be variable too
-const staging = require('../config/environments/staging');
-
-const baseUrl = staging.baseUrl;
+// const staging = require('../config/environments/staging');
+const environment = require('../config/environments/staging');
+const itemProperties = require('../helper/q-item-properties');
+const baseUrl = environment.baseUrl;
 
 const getItem = function(itemId) {
-	const database = staging.database;
- 	console.log('https://nzz-storytelling.cloudant.com/' + database + '/' + itemId);
+	const database = environment.database;
  	return Fetch('https://nzz-storytelling.cloudant.com/' + database + '/' + itemId)
 		.then(response => {
 			return response.json();
@@ -14,8 +14,11 @@ const getItem = function(itemId) {
 			let tool = json.tool;
 			// TODO: let tool decide from which base url data should be fetched -> via config file
 			let body = {};
+			for (var i = 0; i < itemProperties.length; i++) {
+				delete json[itemProperties[i]];
+			}
+			console.log(JSON.stringify(json));			
 			body.item = json;
-			console.log(JSON.stringify(body));
 			return Fetch('https://q-' + tool + '.' + baseUrl + '/static', {
 				method: 'POST',
 				body: JSON.stringify(body),
