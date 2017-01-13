8cbee5ce605293dc6a90f94d995f1f3a375f56fc
Beni <beni.buess@nzz.ch>
2017-01-13 20:09:55 +0100

    fixes routes imports

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const renderingInfoRoute = require('./rendering-info');
-const scriptRoute = require('./tools/stylesheet');
+const scriptRoute = require('./tools/script');
 const stylesheetRoute = require('./tools/stylesheet');
 const versionRoute = require('./version');
 
