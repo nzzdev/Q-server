15ad5934b5b434d07563d90b8923724fdef1bb65
Beni <beni.buess@nzz.ch>
2017-02-21 12:05:24 +0100

    sets meta properties on item POST and PUT before storing to DB

--- routes/item.js
+++ routes/item.js
@@ -27,16 +27,34 @@ module.exports = [
       auth: 'q-auth',
       cors: {
         credentials: true
-      }
+      },
+      description: 'stores a new item to the database and returns the id among other things',
+      tags: ['api']
     },
     handler: (request, reply) => {
       let db = getDb();
-      db.insert(request.payload, (err, doc) => {
+      let doc = request.payload;
+      let now = new Date();
+
+      // docDiff is used to store all the changed properties
+      // to send them back to Q Editor for it to merge it with
+      // the existing item state
+      let docDiff = {}
+
+      doc.createdDate = now.toISOString();
+      docDiff.createdDate = doc.createdDate;
+      doc.createdBy = request.auth.credentials.name;
+      docDiff.createdBy = doc.createdBy;
+
+      db.insert(request.payload, (err, res) => {
         if (err) {
-          console.log(err);
           return reply(Boom.create(err.statusCode, err.description))
         }
-        return reply(doc).type('application/json')
+
+        docDiff._id = res.id;
+        docDiff._rev = res.rev;
+
+        return reply(docDiff).type('application/json')
       })
     }
   },
@@ -51,16 +69,45 @@ module.exports = [
       auth: 'q-auth',
       cors: {
         credentials: true
-      }
+      },
+      description: 'updates an existing item to the database and returns the new revision number among other things',
+      tags: ['api']
     },
     handler: (request, reply) => {
       let db = getDb();
-      // TODO: set the updated date
-      db.insert(request.payload, (err, doc) => {
-        if (err) {
-          return reply(Boom.create(err.statusCode, err.description))
+      let doc = request.payload;
+      let now = new Date();
+      
+      // docDiff is used to store all the changed properties
+      // to send them back to Q Editor for it to merge it with
+      // the existing item state
+      let docDiff = {}
+
+      doc.updatedDate = now.toISOString();
+      doc.updatedBy = request.auth.credentials.name;
+
+      db.get(request.payload._id, (err, oldDoc) => {
+        // if the active state change to true, we set activateDate
+        if (doc.active === true && oldDoc.active === false) {
+          doc.activateDate = now.toISOString();
+          docDiff.activateDate = doc.activateDate;
         }
-        return reply(doc).type('application/json')
+
+        // if the active state change to false, we set activateDate
+        if (doc.active === false && oldDoc.active === true) {
+          doc.deactivateDate = now.toISOString();
+          docDiff.deactivateDate = doc.deactivateDate;
+        }
+
+        db.insert(doc, (err, res) => {
+          if (err) {
+            return reply(Boom.create(err.statusCode, err.description))
+          }
+
+          docDiff._rev = res.rev;
+
+          return reply(docDiff).type('application/json')
+        })
       })
     }
   }
