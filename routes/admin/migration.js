779373eef9e2c3f7c98552dd57f9b0030c3a6cb2
Sharon Funke <sharon.funke@nzz.ch>
2017-03-24 15:46:57 +0100

    Change description of endpoint

--- routes/admin/migration.js
+++ routes/admin/migration.js
@@ -17,7 +17,7 @@ module.exports = {
     cors: {
       credentials: true
     },*/
-    description: 'Executes migration of items in database for specified tool',
+    description: 'Executes migration of items in database for specified tool or for a single item with the specified id respectively',
     tags: ['api']
   },
   handler: (request, reply) => {
