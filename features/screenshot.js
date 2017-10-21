58fbd2579e803eb0c6a6b6e91e6b3331e036abac
Beni <beni.buess@nzz.ch>
2017-10-21 15:13:08 +0200

    launch chrome headless with --no-sandbox to make it work in docker

--- features/screenshot.js
+++ features/screenshot.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function getScreenshot(emptyPageUrl, markup, scripts, stylesheets, config) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   
   const page = await browser.newPage();
   await page.setViewport({
