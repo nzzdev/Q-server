fc4fd8575d0b422dcffd2691ed80fff514e65f58
Beni <beni.buess@nzz.ch>
2017-10-24 15:52:58 +0200

    feat(screenshot): bc compatible implementation of renaming of target.preview to target.context

--- routes/editor/targets.js
+++ routes/editor/targets.js
@@ -8,6 +8,16 @@ module.exports = {
   },
   handler: (request, reply) => {
     const targets = request.server.settings.app.targets.get('');
+    for (let target of targets) {
+      if (target.hasOwnProperty('preview')) {
+        console.log('DEPRECATION NOTICE: target.preview config will be renamed to target.context in Q server 3.0.');
+      } else {
+        // just here for backwards compatibility
+        if (target.hasOwnProperty('context')) {
+          target.preview = target.context;
+        }
+      }
+    }
     reply(targets);
   }
 }
