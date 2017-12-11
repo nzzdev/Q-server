216d9110b7be6daec6c881a972a540ef39edd89c
benib <beni.buess@nzz.ch>
2017-12-11 23:31:28 +0100

    (slack): fix typo

--- plugins/slack/slash-command-routes.js
+++ plugins/slack/slash-command-routes.js
@@ -21,7 +21,7 @@ module.exports = {
             const numberOfItemsPerDayResponse = await server.inject('/statistics/number-of-items-per-day/7');
             const stats = numberOfItemsPerDayResponse.payload;
 
-            const numberOfNewItemsResponse = await server.inect(`/statistics/number-of-items/${Date.now() - (7 * 24 * 60 * 60 * 1000)}`);
+            const numberOfNewItemsResponse = await server.inject(`/statistics/number-of-items/${Date.now() - (7 * 24 * 60 * 60 * 1000)}`);
             const numberOfItemsInTheLast24 = numberOfItemsPerDayResponse.payload;
 
             let message = `*${numberOfItemsInTheLast24}* in the last 7*24 hours\n`;
