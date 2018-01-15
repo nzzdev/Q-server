9b5f211ca486efb3d39137977fd845f3350890db
benib <beni.buess@nzz.ch>
2018-01-15 18:07:35 +0100

    (plugin files): allow to configure CacheControl header for uploaded s3 objects

--- plugins/file/index.js
+++ plugins/file/index.js
@@ -1,14 +1,15 @@
-const AWS = require('aws-sdk');
-const uuid = require('uuid');
-const Mimos = require('mimos');
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
+const Mimos = require("mimos");
 const mimos = new Mimos();
 
 function getDateString() {
   const now = new Date();
   const day = now.getDate() < 10 ? `0${now.getDate()}` : now.getDate();
-  const month = now.getMonth() < 9 ? `0${now.getMonth() + 1}` : now.getMonth() + 1;
+  const month =
+    now.getMonth() < 9 ? `0${now.getMonth() + 1}` : now.getMonth() + 1;
   return `${now.getFullYear()}/${month}/${day}`;
-};
+}
 
 async function upload(s3, params) {
   return new Promise((resolve, reject) => {
@@ -22,14 +23,15 @@ async function upload(s3, params) {
 }
 
 module.exports = {
-  name: 'q-file',
+  name: "q-file",
   register: async function(server, options) {
-
-    if (!options.s3AccessKey ||
-        !options.s3SecretKey ||
-        !options.s3Region ||
-        !options.s3Bucket) {
-      throw new Error('Not all required S3 options are provided.');
+    if (
+      !options.s3AccessKey ||
+      !options.s3SecretKey ||
+      !options.s3Region ||
+      !options.s3Bucket
+    ) {
+      throw new Error("Not all required S3 options are provided.");
     }
 
     const s3 = new AWS.S3({
@@ -38,56 +40,69 @@ module.exports = {
       region: options.s3Region
     });
 
-    const contentTypes = options.contentTypes || ['application/pdf', 'image/jpeg', 'image/png'];
+    const contentTypes = options.contentTypes || [
+      "application/pdf",
+      "image/jpeg",
+      "image/png"
+    ];
 
     server.route({
-      method: 'POST',
-      path: '/file',
+      method: "POST",
+      path: "/file",
       options: {
-        auth: 'q-auth',
+        auth: "q-auth",
         cors: {
           credentials: true,
-          additionalHeaders: ["x-requested-with", "x-file-name", "cache-control"]
+          additionalHeaders: [
+            "x-requested-with",
+            "x-file-name",
+            "cache-control"
+          ]
         },
-        tags: ['api'],
+        tags: ["api"],
         payload: {
-          output: 'stream',
+          output: "stream",
           parse: true,
-          allow: 'multipart/form-data',
+          allow: "multipart/form-data",
           maxBytes: 10485760
         }
       },
       handler: async function(request, h) {
         const file = request.payload.file;
         if (!file) {
-          return Boom.badData('Failed to read file');
+          return Boom.badData("Failed to read file");
         }
-        
-        const contentType = file.hapi.headers['content-type'];
+
+        const contentType = file.hapi.headers["content-type"];
         if (contentTypes.indexOf(contentType) === -1) {
-          return Boom.unsupportedMediaType('Content-type not allowed');
+          return Boom.unsupportedMediaType("Content-type not allowed");
         }
 
         const type = mimos.type(contentType);
 
-        const extension = type.extensions[0] || '';
+        const extension = type.extensions[0] || "";
         const filename = `${uuid.v4()}.${extension}`;
 
         const fileKey = `${getDateString()}/${filename}`;
 
-        const data = await upload(s3, {
+        const params = {
           Bucket: options.s3Bucket,
           Key: fileKey,
           Body: file,
           ContentType: contentType
-        });
+        };
+
+        if (options.cacheControl) {
+          params.CacheControl = options.cacheControl;
+        }
+
+        const data = await upload(s3, params);
 
         return {
           key: data.key,
-          url: data.Location 
+          url: data.Location
         };
       }
-    })
-
+    });
   }
-}
+};
