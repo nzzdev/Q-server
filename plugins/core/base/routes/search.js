e0433443ee49fe7af0790a399112e2002e4eaaeb
Manuel Roth <manuelroth@hotmail.ch>
2018-03-28 17:07:02 +0200

    (search) use server generated mango query to search in the items db in /search
    * Prettier formatting
    
    * Add new search route based on mango query language
    
    * Correct nano method
    
    * Only sort by updatedDate
    
    * Allow tool parameter to be either string or array
    
    * Fix tool parameter validation
    
    * Refactor selector generation
    
    * Constify more variables
    
    * Remove old post search route
    
    * Replace for loops with map inside getSearchFilters
    
    * Fix filterParameter construction
    
    * Refactor search server methode parameters
    
    * Use extracted variables limit and bookmark

--- plugins/core/base/routes/search.js
+++ plugins/core/base/routes/search.js
@@ -1,16 +1,33 @@
-const Boom = require('boom');
-const Joi = require('joi');
+const Boom = require("boom");
+const Joi = require("joi");
 
 module.exports = {
-  path: '/search',
-  method: 'POST',
+  path: "/search",
+  method: "GET",
   options: {
     validate: {
-      payload: Joi.object().required()
+      query: {
+        limit: Joi.number().optional(),
+        bookmark: Joi.string().optional(),
+        tool: Joi.alternatives()
+          .try(Joi.array().items(Joi.string()), Joi.string())
+          .optional(),
+        createdBy: Joi.string().optional(),
+        department: Joi.string().optional(),
+        publication: Joi.string().optional(),
+        active: Joi.boolean().optional(),
+        searchString: Joi.string().optional()
+      }
     },
-    tags: ['api', 'editor']
+    tags: ["api", "editor"]
   },
   handler: async (request, h) => {
-    return request.server.methods.db.item.search(request.payload);
+    // Creates new object filterProperties which contains all properties but bookmark and limit
+    const { bookmark, limit, ...filterProperties } = request.query;
+    return request.server.methods.db.item.search(
+      filterProperties,
+      limit,
+      bookmark
+    );
   }
-}
+};
--- plugins/core/db/index.js
+++ plugins/core/db/index.js
@@ -1,11 +1,47 @@
-const nano = require('nano');
-const Boom = require('boom');
+const nano = require("nano");
+const Boom = require("boom");
+
+function getSearchFilters(filterProperties) {
+  return Object.keys(filterProperties).map(parameterName => {
+    const parameterValue = filterProperties[parameterName];
+    if (parameterName === "searchString") {
+      const searchFields = ["id", "title", "subtitle", "annotations"];
+      return {
+        $or: searchFields.map(searchField => {
+          const searchStringFilter = {};
+          searchStringFilter[searchField] = {
+            $regex: parameterValue + "*"
+          };
+          return searchStringFilter;
+        })
+      };
+    }
+    if (parameterName === "tool" && Array.isArray(parameterValue)) {
+      return {
+        $or: parameterValue.map(tool => {
+          return {
+            tool: {
+              $eq: tool
+            }
+          };
+        })
+      };
+    }
+    const filter = {};
+    filter[parameterName] = {
+      $eq: parameterValue
+    };
+    return filter;
+  });
+}
 
 module.exports = {
-  name: 'q-db',
-  register: async function (server, options) {
-    const dbUrl = `${options.protocol || 'https'}://${options.host}/${options.database}`;
-    server.log(['info'], `Connecting to database ${dbUrl}`);
+  name: "q-db",
+  register: async function(server, options) {
+    const dbUrl = `${options.protocol || "https"}://${options.host}/${
+      options.database
+    }`;
+    server.log(["info"], `Connecting to database ${dbUrl}`);
 
     const nanoConfig = {
       url: dbUrl
@@ -17,34 +53,36 @@ module.exports = {
           user: options.user,
           pass: options.pass
         }
-      }
+      };
     }
 
     server.app.db = nano(nanoConfig);
 
-    server.method('db.item.getById', function(id, ignoreInactive = false) {
+    server.method("db.item.getById", function(id, ignoreInactive = false) {
       return new Promise((resolve, reject) => {
         server.app.db.get(id, (err, item) => {
           if (err) {
-            return reject(new Boom(err.description, { statusCode: err.statusCode } ));
+            return reject(
+              new Boom(err.description, { statusCode: err.statusCode })
+            );
           }
-      
+
           if (!ignoreInactive && item.active !== true) {
-            return reject(Boom.forbidden('Item is not active'));
+            return reject(Boom.forbidden("Item is not active"));
           }
           return resolve(item);
-        })
+        });
       });
     });
 
-    server.method('db.item.getAllByTool', function(tool) {
+    server.method("db.item.getAllByTool", function(tool) {
       const options = {
         keys: [tool],
         include_docs: true,
         reduce: false
       };
       return new Promise((resolve, reject) => {
-        server.app.db.view('items', 'byTool', options, async(err, data) => {
+        server.app.db.view("items", "byTool", options, async (err, data) => {
           if (err) {
             return reject(Boom.internal(err));
           } else {
@@ -53,19 +91,41 @@ module.exports = {
             });
             return resolve(items);
           }
-        })
+        });
       });
     });
 
-    server.method('db.item.search', function(payload) {
+    server.method("db.item.search", function(
+      filterProperties,
+      limit,
+      bookmark
+    ) {
       return new Promise((resolve, reject) => {
-        server.app.db.search('items', 'search', payload, (err, data) => {
+        const requestOptions = {
+          db: options.database,
+          path: "_find",
+          method: "POST",
+          body: {
+            selector: {
+              $and: getSearchFilters(filterProperties)
+            },
+            sort: [
+              {
+                updatedDate: "desc"
+              }
+            ],
+            limit: limit || 18,
+            bookmark: bookmark || null
+          }
+        };
+
+        server.app.db.server.request(requestOptions, (err, data) => {
           if (err) {
             return reject(Boom.internal(err));
           } else {
             return resolve(data);
           }
-        })
+        });
       });
     });
   }
