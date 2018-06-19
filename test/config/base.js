489870161d43682af5b8af8b8f7e68dcab7cc1b9
benib <beni.buess@nzz.ch>
2018-06-19 13:29:11 +0400

    Cache-Control: public is not default anymore but needs to be in config

--- test/config/base.js
+++ test/config/base.js
@@ -1,25 +1,27 @@
-const Confidence = require('confidence');
+const Confidence = require("confidence");
 
 const base = {
   cache: {
-    cacheControl: { // these are the default cache-control headers used for the tool-default route in case a tool is not responding with it's own directives.
+    cacheControl: {
+      // these are the default cache-control headers used for the tool-default route in case a tool is not responding with it's own directives.
+      public: true,
       maxAge: 1,
       sMaxAge: 1,
       staleWhileRevalidate: 1,
       staleIfError: 1
     }
   }
-}
+};
 
-const env = process.env.APP_ENV || 'local';
+const env = process.env.APP_ENV || "local";
 const store = new Confidence.Store(base);
 
 module.exports.get = (key, criteria = {}) => {
-  criteria = Object.assign({ env: env }, criteria)
-  return store.get(key, criteria)
-}
+  criteria = Object.assign({ env: env }, criteria);
+  return store.get(key, criteria);
+};
 
 module.exports.meta = (key, criteria = {}) => {
-  criteria = Object.assign({ env: env }, criteria)
-  return store.meta(key, criteria)
-}
+  criteria = Object.assign({ env: env }, criteria);
+  return store.meta(key, criteria);
+};
--- test/config/rendering-info.js
+++ test/config/rendering-info.js
@@ -1,25 +1,29 @@
-const Confidence = require('confidence');
+const Confidence = require("confidence");
 
 const renderingInfoConfig = {
   cache: {
-    cacheControl: { // these are the default cache-control headers in case a tool is not responding with it's own directives.
+    cacheControl: {
+      // these are the default cache-control headers in case a tool is not responding with it's own directives.
+      public: true,
       maxAge: {
-        $filter: 'env',
+        $filter: "env",
         local: 1,
         $default: 60
       },
       sMaxAge: {
-        $filter: 'env',
+        $filter: "env",
         local: 1,
         $default: 60
       },
-      staleWhileRevalidate: { // this is just for the CDN, it will serve maximum 24 hours old content while revalidating in the background
-        $filter: 'env',
+      staleWhileRevalidate: {
+        // this is just for the CDN, it will serve maximum 24 hours old content while revalidating in the background
+        $filter: "env",
         local: 1,
         $default: 60 * 60 * 24
       },
-      staleIfError: {  // this is just for the CDN, it will serve maximum 7 days old content if the backend is not running
-        $filter: 'env',
+      staleIfError: {
+        // this is just for the CDN, it will serve maximum 7 days old content if the backend is not running
+        $filter: "env",
         local: 1,
         $default: 60 * 60 * 24 * 7
       }
@@ -27,20 +31,20 @@ const renderingInfoConfig = {
   },
   toolRuntimeConfig: {
     foo: {
-      bar: 'baz'
+      bar: "baz"
     }
   }
-}
+};
 
-const env = process.env.APP_ENV || 'local';
+const env = process.env.APP_ENV || "local";
 const store = new Confidence.Store(renderingInfoConfig);
 
 module.exports.get = (key, criteria = {}) => {
-  criteria = Object.assign({ env: env }, criteria)
-  return store.get(key, criteria)
-}
+  criteria = Object.assign({ env: env }, criteria);
+  return store.get(key, criteria);
+};
 
 module.exports.meta = (key, criteria = {}) => {
-  criteria = Object.assign({ env: env }, criteria)
-  return store.meta(key, criteria)
-}
+  criteria = Object.assign({ env: env }, criteria);
+  return store.meta(key, criteria);
+};
