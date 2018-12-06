1507ce34035be73eb01760ae58365a78b4be352e
benib <beni.buess@nzz.ch>
2018-12-06 11:46:35 +0100

    (chore) prettier code formatting

--- test/server.js
+++ test/server.js
@@ -1,11 +1,11 @@
-const Hapi = require('hapi');
+const Hapi = require("hapi");
 
 function getServer() {
   let server = Hapi.server({
     port: process.env.PORT || 3333,
     app: {
-      tools: require('./config/tools.js'),
-      targets: require('./config/targets.js'),
+      tools: require("./config/tools.js"),
+      targets: require("./config/targets.js")
     },
     routes: {
       cors: true
@@ -13,15 +13,14 @@ function getServer() {
   });
 
   // mock the auth strategy
-  server.auth.scheme('mock', function(server, options) {
+  server.auth.scheme("mock", function(server, options) {
     return {
       authenticate: function(request, h) {
-        debugger;
-        return {credentials: 'user'};
+        return { credentials: "user" };
       }
     };
   });
-  server.auth.strategy('q-auth', 'mock');
+  server.auth.strategy("q-auth", "mock");
   return server;
 }
 
