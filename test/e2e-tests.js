760311d67f9c610d9d9ca60b4bc732542f150d41
Michel Grautstueck <michel.grautstueck@nzz.ch>
2022-12-05 16:46:11 +0100

    Increase test timeout

--- test/e2e-tests.js
+++ test/e2e-tests.js
@@ -678,7 +678,7 @@ lab.experiment("core schema endpoints", () => {
 lab.experiment("screenshot plugin", () => {
   it(
     "returns a screenshot with correct cache-control headers with wait as number",
-    { timeout: 5000, plan: 3 },
+    { timeout: 10000, plan: 3 },
     async () => {
       const response = await server.inject(
         "/screenshot/mock-item-active.png?target=pub1&width=500&wait=200"
@@ -693,7 +693,7 @@ lab.experiment("screenshot plugin", () => {
 
   it(
     "returns a screenshot with correct cache-control headers with wait as selector",
-    { timeout: 5000, plan: 3 },
+    { timeout: 10000, plan: 3 },
     async () => {
       const response = await server.inject(
         "/screenshot/mock-item-active.png?target=pub1&width=500&wait=h1"
@@ -708,7 +708,7 @@ lab.experiment("screenshot plugin", () => {
 
   it(
     "returns a screenshot with correct cache-control headers without wait",
-    { timeout: 5000, plan: 3 },
+    { timeout: 10000, plan: 3 },
     async () => {
       const response = await server.inject(
         "/screenshot/mock-item-active.png?target=pub1&width=500"
